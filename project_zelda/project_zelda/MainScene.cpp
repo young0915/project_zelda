151fb00c122416b4bbbcfb793d962a5af665082f
ungs0915 <ungs0915@gmail.com>
2024-12-01 22:54:26 +0900

    [클라] 1. 인트로 추가

--- project_zelda/project_zelda/MainScene.cpp
+++ project_zelda/project_zelda/MainScene.cpp
@@ -1,5 +1,10 @@
 #include "pch.h"
 #include "MainScene.h"
+#include "ResourceManager.h"
+#include "SceneManager.h"
+#include "InputManager.h"
+#include "SpriteActor.h"
+#include "Sprite.h"
 
 MainScene::MainScene()
 {
@@ -11,6 +16,22 @@ MainScene::~MainScene()
 
 void MainScene::Init()
 {
+
+	GET_SINGLE(ResourceManager)->LoadTexture(L"Intro", L"Sprite\\UI\\Intro.bmp");
+	GET_SINGLE(ResourceManager)->CreateSprite(L"Intro", GET_SINGLE(ResourceManager)->GetTexture(L"Intro"));
+
+	{
+		Sprite* sprite = GET_SINGLE(ResourceManager)->GetSprite(L"Intro");
+
+		SpriteActor* background = new SpriteActor();
+		background->SetSprite(sprite);
+		background->SetLayer(LAYER_BACKGROUND);
+		const Vec2Int size = sprite->GetSize();
+		background->SetPos(Vec2(size.x / 2, size.y / 2));
+
+		AddActor(background);
+	}
+
 	Super::Init();
 
 }
@@ -19,6 +40,11 @@ void MainScene::Update()
 {
 	Super::Update();
 
+	// 아무 키 눌러서 입장할 것
+	if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::LeftMouse))
+	{
+		GET_SINGLE(SceneManager)->ChangeScene(SceneType::BattleScene);
+	}
 }
 
 void MainScene::Render(HDC hdc)
